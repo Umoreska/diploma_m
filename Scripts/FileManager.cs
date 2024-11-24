dde244b9821415eba2390454ca614792a3508d14
Andrii Kharchenko <mrander7443@gmail.com>
2024-11-24 11:26:10 +0200

    file manager created

---
+++ Scripts/FileManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using SimpleFileBrowser;
+public class FileManager : MonoBehaviour
+{
+    
+    void Start()
+    {
+       
+    }
+}
--- Scripts/UIController.cs
+++ Scripts/UIController.cs
@@ -8,6 +8,7 @@ using UnityEditor.Formats.Fbx.Exporter;
 using Autodesk.Fbx;
 using UnityEditor;
 using Unity.IntegerTime;
+using SimpleFileBrowser;
 
 
 public class UIController : MonoBehaviour
@@ -180,6 +181,53 @@ public class UIController : MonoBehaviour
 
             placed_objects[i].transform.localScale = mesh_transform.localScale;
         }
+    }
+
+    public void ShowSaveDialog() {
+         // Set filters (optional)
+		// It is sufficient to set the filters just once (instead of each time before showing the file browser dialog), 
+		// if all the dialogs will be using the same filters
+		FileBrowser.SetFilters( true, new FileBrowser.Filter( "Images", ".jpg", ".png" ), new FileBrowser.Filter( "Text Files", ".txt", ".pdf" ) );
+
+		// Set default filter that is selected when the dialog is shown (optional)
+		// Returns true if the default filter is set successfully
+		// In this case, set Images filter as the default filter
+		FileBrowser.SetDefaultFilter( ".fbx" );
+
+		// Set excluded file extensions (optional) (by default, .lnk and .tmp extensions are excluded)
+		// Note that when you use this function, .lnk and .tmp extensions will no longer be
+		// excluded unless you explicitly add them as parameters to the function
+		FileBrowser.SetExcludedExtensions( ".lnk", ".tmp", ".zip", ".rar", ".exe" );
+
+		// Add a new quick link to the browser (optional) (returns true if quick link is added successfully)
+		// It is sufficient to add a quick link just once
+		// Name: Users
+		// Path: C:\Users
+		// Icon: default (folder icon)
+		FileBrowser.AddQuickLink( "Users", "C:\\Users", null );
+		
+		// !!! Uncomment any of the examples below to show the file browser !!!
+
+		// Example 1: Show a save file dialog using callback approach
+		// onSuccess event: not registered (which means this dialog is pretty useless)
+		// onCancel event: not registered
+		// Save file/folder: file, Allow multiple selection: false
+		// Initial path: "C:\", Initial filename: "Screenshot.png"
+		// Title: "Save As", Submit button text: "Save"
+		FileBrowser.ShowSaveDialog( null, null, FileBrowser.PickMode.Folders, false, "C:\\", "terrain.fbx", "Save As", "Save" );
+
+		// Example 2: Show a select folder dialog using callback approach
+		// onSuccess event: print the selected folder's path
+		// onCancel event: print "Canceled"
+		// Load file/folder: folder, Allow multiple selection: false
+		// Initial path: default (Documents), Initial filename: empty
+		// Title: "Select Folder", Submit button text: "Select"
+		// FileBrowser.ShowLoadDialog( ( paths ) => { Debug.Log( "Selected: " + paths[0] ); },
+		//						   () => { Debug.Log( "Canceled" ); },
+		//						   FileBrowser.PickMode.Folders, false, null, null, "Select Folder", "Select" );
+
+		// Example 3: Show a select file dialog using coroutine approach
+		// StartCoroutine( ShowLoadDialogCoroutine() );
     }
 
     public void ExportMeshToFbx() {
